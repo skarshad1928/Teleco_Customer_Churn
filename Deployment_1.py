9572610c23f338ff56a9fbb590083986400b2e7d
SHAIK ARSHAD <arshad.23bce9489@vitapstudent.ac.in>
2025-11-05 09:09:53 +0530

    Update Deployment_1.py

--- Deployment_1.py
+++ Deployment_1.py
@@ -54,8 +54,8 @@ elif page == "Data Exploration":
         plt.figure(figsize=(10, 6))
         sns.heatmap(numeric_df.corr(), annot=True, cmap="RdBu", center=0)
         st.pyplot(plt.gcf())
-        st.markdown("**Resources:** [Data_understanding_explore](https://github.com/skarshad1928/Python/blob/main/Data_ware_House_Workspace/WORK1.ipynb)")
-        st.markdown("**Resources:** [Data_prep_for_model](https://github.com/skarshad1928/Python/blob/main/Data_ware_House_Workspace/WORK2.ipynb)")
+    st.markdown("**Resources:** [Data_understanding_explore](https://github.com/skarshad1928/Python/blob/main/Data_ware_House_Workspace/WORK1.ipynb)")
+    st.markdown("**Resources:** [Data_prep_for_model](https://github.com/skarshad1928/Python/blob/main/Data_ware_House_Workspace/WORK2.ipynb)")
 
 elif page == "Model Training":
     st.title("Model Training - Logistic Regression")
