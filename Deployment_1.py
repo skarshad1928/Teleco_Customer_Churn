6266bd5531437d66da5cb378a4b96dd037a76402
SHAIK ARSHAD <arshad.23bce9489@vitapstudent.ac.in>
2025-11-05 11:32:02 +0530

    Update Deployment_1.py

--- Deployment_1.py
+++ Deployment_1.py
@@ -60,6 +60,7 @@ if page == "Project Overview":
     It demonstrates end-to-end implementation of a real-world telecom churn prediction system — from data exploration and modeling to visualization and deployment.  
     The workflow, presentation, and reproducibility of results reflect a professional, research-grade project designed to meet academic and industry expectations.  
     """)
+    st.markdown("**Resources:** [Data_source](https://www.kaggle.com/datasets/blastchar/telco-customer-churn)")
 
     st.markdown("**Resources:** [GitHub Repository](https://github.com/skarshad1928/Teleco_Customer_Churn/tree/main)")
 
